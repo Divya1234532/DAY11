921564d49dd763eb690927939b033af09553c417
Divya1234532 <dd1320983@gmail.com>
2026-04-01 09:13:23 +0530

    Create ExceptionHandling.java

---
+++ ExceptionHandling.java
@@ -0,0 +1,30 @@
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        try
+        {
+            /*int a=8;
+            int b=0;
+            int c=a/b;
+             */
+            String s=null;
+            System.out.println(s.length());
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("Number is divided by zero");
+        }
+        catch(NullPointerException e)
+        {
+            System.out.println("String value is null");
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        finally
+        {
+            System.out.println("Finally block always execute is try or catch not working means");
+        }
+    }
+}
